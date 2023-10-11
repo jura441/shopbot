45b16e115b565b9aa92591618cf7a5285175eda1
jura441 <102172272+jura441@users.noreply.github.com>
2023-10-11 20:27:06 +0300

    Add files via upload

---
+++ telegrams-master/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Button from './Components/Button/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Hi i am shopoBot</h1>
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
